6985733f15f2c36b9570c7fa5ac9b884f86c1f8f
sadan <117494111+sadan4@users.noreply.github.com>
2024-08-02 02:07:09 -0400

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import { LogoMap } from "./symbols";
  * @param func The function to wrap
  * @param delay The delay in milliseconds
  */
-export function debounce<T extends Function>(func: T, delay = 300): T {
+function debounce<T extends Function>(func: T, delay = 300): T {
     let timeout: NodeJS.Timeout;
     return function (...args: any[]) {
         clearTimeout(timeout);
